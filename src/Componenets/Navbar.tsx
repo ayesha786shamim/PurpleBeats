2c6c83889d0d3c5128b810ff4c822a868871f4fe
Ayesha Shamim <ayeshashamim.011@gmail.com>
2025-07-11 17:07:59 +0500

    Add Navbar.tsx

---
+++ src/Componenets/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/player">Player</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
